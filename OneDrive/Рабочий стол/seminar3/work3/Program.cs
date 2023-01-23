cd9b5f4d2a707a9fb14cb7f6538d08768642c8a9
Your Name <you@example.com>
2023-01-23 23:26:28 +0500

    3

---
+++ OneDrive/Рабочий стол/seminar3/work3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+System.Console.WriteLine("ввод числа");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"num = {num}");
+
+for( int i = 1; i <= num; i++)
+{
+    double result = (Math.Pow( i, 3));
+    System.Console.WriteLine(result);
+}
