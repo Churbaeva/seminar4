87f084e257a80fdac77372501c1e71617e774264
Your Name <you@example.com>
2023-01-23 23:24:42 +0500

    1

---
+++ OneDrive/Рабочий стол/seminar3/work1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+string a= string.Empty;
+Console.WriteLine("введите число");
+a = Console.ReadLine();
+char[]temp = a.ToCharArray();
+Array.Reverse(temp);
+string b = new string(temp);
+
+if (a.ToLower().Equals(b.ToLower()))
+{
+System.Console.WriteLine("yes");
+}
+else 
+{
+    System.Console.WriteLine("no");
+} 
