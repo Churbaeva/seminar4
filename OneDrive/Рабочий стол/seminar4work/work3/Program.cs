8a2f906d75b205b588a6442a51b1a9d160c89a28
Your Name <you@example.com>
2023-01-31 10:26:57 +0500

    5

---
+++ OneDrive/Рабочий стол/seminar4work/work3/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их 
+//на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+Console.Write("ввод массива a ");
+int a= Convert.ToInt32(Console.ReadLine());
+int[] arr = GetRandomArray(8);
+PrintArray(arr);
+int[] GetRandomArray(int Lenght)
+{
+int[] result = new int[Lenght];
+
+    for (int i=0; i<Lenght; i++)
+    {
+        Console.Write(" ");
+        result[i]=new Random().Next(1, 9);
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach(int item in arr)
+    
+    {
+        Console.Write(item + " ");
+    }
+}
+
