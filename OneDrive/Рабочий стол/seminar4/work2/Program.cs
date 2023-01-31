fa3a51759a8e2f69ebc701f5cda4888cff84f410
Your Name <you@example.com>
2023-01-31 09:50:48 +0500

    work22

---
+++ OneDrive/Рабочий стол/seminar4/work2/Program.cs
@@ -0,0 +1,37 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int number = ReadInt("Введите число: ");
+int count = CountNum(number);
+SumNumbers(number, count);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int CountNum(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+
+void SumNumbers(int n, int count)
+{
+    int sum = 0;
+    for (int i = 1; i <= count; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
